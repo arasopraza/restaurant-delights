1d4b4637ce2754bcfa9fce993294afcc624984be
arasopraza <aras.opraza@gmail.com>
2024-07-23 11:09:34 +0700

    feat: add home view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+
+from django.views.generic import TemplateView
+
+from .models import Ingredient, MenuItem, Purchase
 
 # Create your views here.
+class HomeView(TemplateView):
+  template_name = "restaurant/home.html"
+
+  def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["ingredients"] = Ingredient.objects.all()
+        context["menu_items"] = MenuItem.objects.all()
+        context["purchases"] = Purchase.objects.all()
+        return context
